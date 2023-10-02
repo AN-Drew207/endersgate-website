fc51fb705e402262c30cb1ec074a317528352fd1
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2023-10-02 16:17:48 -0400

    changed competitive page to marketplace

--- src/app/comics/page.tsx
+++ src/app/comics/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import React from "react";
 
-const Inventory = () => {
+const Comics = () => {
   React.useEffect(() => {
     window.location.replace("https://marketplace.endersgate.gg/comics");
   });
 
   return (
     <div className="flex flex-col w-full min-h-screen items-center justify-center px-28 text-center text-white font-bold text-xl bg-black">
-      We are migrating our inventory to
-      https://marketplace.endersgate.gg/comics. <br /> Redirecting you now...
+      We are migrating our comics to https://marketplace.endersgate.gg/comics.{" "}
+      <br /> Redirecting you now...
     </div>
   );
 };
 
-export default Inventory;
+export default Comics;
--- src/app/competitive/page.tsx
+++ src/app/competitive/page.tsx
@@ -1,8 +1,18 @@
 "use client";
-import CompetitiveComponent from "@/components/newComponents/Competitive/Competitive";
+import React from "react";
 
-const CompetitivePage = () => {
-  return <CompetitiveComponent />;
+const Competitive = () => {
+  React.useEffect(() => {
+    window.location.replace("https://marketplace.endersgate.gg/competitive");
+  });
+
+  return (
+    <div className="flex flex-col w-full min-h-screen items-center justify-center px-28 text-center text-white font-bold text-xl bg-black">
+      We are migrating our competitive page to
+      https://marketplace.endersgate.gg/competitive. <br /> Redirecting you
+      now...
+    </div>
+  );
 };
 
-export default CompetitivePage;
+export default Competitive;
