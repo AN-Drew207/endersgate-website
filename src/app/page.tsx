e1b32f9edd90ece112f28d8e5f6b0537a05b80c6
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2024-01-03 15:51:21 -0400

    added new video to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ function LandingPage() {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/ifrnpcZtpK4"
+              src="https://www.youtube.com/embed/YmxknzO-2-k?si=LxQs_TnxbbjeRIbz"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
